c8d693f40c609a1d8a1d8f0bbba5a0dcf72eef7a
hailaicon <169118104+hailaicon@users.noreply.github.com>
2024-05-29 13:56:44 +0700

    update map4

--- Assets/Scene 4/Script/Player.cs
+++ Assets/Scene 4/Script/Player.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player1 : MonoBehaviour
 {
     public Rigidbody2D qf;
     public bool jump;
     public Animator ani;
+    public int mang = 3;
+    public Text lifetext;
+    public Transform checkpoint;
+    public bool climbing;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,9 +35,19 @@ public class Player1 : MonoBehaviour
             ani.SetBool("Speed", true);
         }
         else ani.SetBool("Speed", false);
-        if (Input.GetKeyDown(KeyCode.W)&&jump)
+        if (Input.GetKeyDown(KeyCode.W) && jump)
         {
             qf.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+        }
+        if(mang < 1)
+        {
+            Destroy(this.gameObject);
+        }
+        if (climbing)
+        {
+            var leothang = Input.GetAxisRaw("Horizontal");
+            var leothang2 = Input.GetAxisRaw("Vertical");
+            qf.velocity = new Vector3(leothang *1f, leothang2 *3f, 0);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -42,13 +57,37 @@ public class Player1 : MonoBehaviour
             jump = true;
             ani.SetBool("Force", false);
         }
+        if (collision.gameObject.CompareTag("enemy"))
+        {
+            mang -= 1;
+            lifetext.text = "Life " + mang;
+            transform.position = checkpoint.position;
+        }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             jump = false;
-            ani.SetBool("Force", true);
+            ani.SetBool("Force",true);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("lander"))
+        {
+            qf.gravityScale = 0;
+            climbing = true;
+            ani.SetBool("climbing", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("lander"))
+        {
+            qf.gravityScale = 1f;
+            climbing = false;
+            ani.SetBool("climbing",false);
         }
     }
 }
