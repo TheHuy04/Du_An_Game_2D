1c6c72e995698c1936aa8a01024f67e68d73d432
TheHuy04 <huyparma123@gmail.com>
2024-05-30 22:44:33 +0700

    xong dich chuyen

--- Assets/Scenes 3/Scripts/Cong.cs
+++ Assets/Scenes 3/Scripts/Cong.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class Cong : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform diemDichChuyen;
+    public Transform getDiemDichChuyen()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        return diemDichChuyen;
     }
 }
