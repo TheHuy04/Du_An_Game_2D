4790ce38390f3d488633160c8f90d5eedd15028a
TheHuy04 <huyparma123@gmail.com>
2024-06-16 13:19:20 +0700

    ok

--- Assets/Scenes 3/Scripts/AudioManager.cs
+++ Assets/Scenes 3/Scripts/AudioManager.cs
@@ -16,6 +16,7 @@ public class AudioManager : MonoBehaviour
     public AudioClip attackClip;
     public AudioClip monsterDie;
     public AudioClip bossDie;
+    public AudioClip Health;
 
     // Start is called before the first frame update
     void Start()
